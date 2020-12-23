4642b548b2dab40840d7600ef891c2fcce50e342
Kovek <cristian8661@gmail.com>
2020-12-22 22:51:26 -0300

    todos terminados

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import './styles.css';
 export const todoList = new TodoList();
 
 todoList.todos.forEach(todo => crearTodoHtml( todo ));
+const newTodo = new Todo('Aprende JS');
+todoList.todos[0].imprimirClase();
+
+console.log( 'todos', todoList.todos);
 // const tarea =  new Todo('Aprender JS!!');
 
 // console.log(todoList)
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -2,9 +2,12 @@
 
 import { Todo } from "../classes";
 import { todoList } from '../index.js';
-const divTodoList = document.querySelector('.todo-list');
-const txtInput    = document.querySelector('.new-todo');
-const btnBorrar = document.querySelector('.clear-completed');
+
+const divTodoList   = document.querySelector('.todo-list');
+const txtInput      = document.querySelector('.new-todo');
+const btnBorrar     = document.querySelector('.clear-completed');
+const ulFilters     = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
 export const crearTodoHtml = ( todo ) => {
 
@@ -72,3 +75,31 @@ btnBorrar.addEventListener('click', (event)=>{
         }
     }
 });
+
+ulFilters.addEventListener('click', ( event )=> { 
+
+    const filtro = event.target.text;
+    if( !filtro ) { return; }
+    anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+
+    for( const elemento of divTodoList.children ) {
+        
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+        switch( filtro ) {
+            case 'Pendientes':
+                if( completado ){
+                    elemento.classList.add('hidden');
+                }
+                break;
+            
+            case 'Completados':
+                if( !completado ){
+                    elemento.classList.add('hidden');
+                }
+                break;
+        }
+    }
+} )
